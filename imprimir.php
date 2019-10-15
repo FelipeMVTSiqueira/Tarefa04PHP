6dd4df0f63f75ed6b7ce3ba6ffe94fef1f7eadbb
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-15 13:27:27 -0300

    Resolução Ex 2a)

--- imprimir.php
+++ imprimir.php
@@ -1,3 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <form action="imprimir.php" method="get">
+            Nome: <input type="text" name="nome"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+
 <?php 
-    var_dump($_GET);
+    var_dump ($_GET);
 ?>
