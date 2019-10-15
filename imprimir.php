ead999ad58cf209d5b4e4aaf13521cfc1e3525a7
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-15 14:17:38 -0300

    Resolução Ex 4a)

--- imprimir.php
+++ imprimir.php
@@ -4,6 +4,23 @@
         <form action="imprimir.php" method="post">
             Nome: <input type="text" name="nome"><br>
             E-mail: <input type="text" name="email"><br>
+            Como ficou sabendo deste site?<br>
+            <div>
+                <input type="checkbox" name="caminho" value="amigo">
+                <label for="caminho">Um amigo me contou!</label>
+            </div>
+            <div>
+                <input type="checkbox" name="caminho" value="google">
+                <label for="caminho">Encontrei no Google!</label>
+            </div>
+            <div>
+                <input type="checkbox" name="caminho" value="naosei">
+                <label for="caminho">Sem querer... mas resolveu!</label>
+            </div>
+            <div>
+                <input type="checkbox" name="caminho" value="nervosinho">
+                <label for="caminho">Não é da sua conta, inxirido!</label>
+            </div>
             <input type="submit">
         </form>
     </body>
